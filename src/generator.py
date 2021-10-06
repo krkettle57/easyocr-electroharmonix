689a27f4e5daf596c4bcf0d1ccfd96b574e91fc7
krkettle57 <krkettle57@gmail.com>
2021-10-07 00:48:32 +0900

    add text image generator

---
+++ src/generator.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from typing import Tuple, Union
+
+from PIL import Image, ImageDraw, ImageFont
+
+Color = Union[float, Tuple[float, ...], str]
+
+
+@dataclass
+class TextImageGenerator:
+    font_filepath: str = "fonts/ipaexg.ttf"
+    fontsize: int = 48
+    mx_ratio: float = 0.1
+    my_ratio: float = 0.1
+
+    def generate(
+        self,
+        text: str,
+        color: Color = (0, 0, 0),
+        bg_color: Color = (255, 255, 255),
+    ) -> Image:
+        font = self._get_font()
+        canvas_size, text_tl = self._get_canvas_size(text, font)
+        img = self._get_canvas(bg_color, canvas_size)
+        draw = ImageDraw.Draw(img)
+        draw.text(text_tl, text, fill=color, font=font)
+
+        return img
+
+    def _get_canvas_size(self, text: str, font: ImageFont.FreeTypeFont) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+        text_width, text_height = ImageDraw.Draw(Image.new("RGB", (200, 200))).textsize(text, font)
+        width = int(text_width * (1 + 2 * self.mx_ratio))
+        height = int(text_height * (1 + 2 * self.my_ratio))
+
+        text_top_left_x = int(width * self.mx_ratio / (1 + 2 * self.mx_ratio))
+        text_top_left_y = int(height * self.my_ratio / (1 + 2 * self.my_ratio))
+        return (width, height), (text_top_left_x, text_top_left_y)
+
+    def _get_canvas(self, bg_color: Color, canvas_size: Tuple[int, int]) -> Image:
+        return Image.new("RGB", canvas_size, bg_color)
+
+    def _get_font(self) -> ImageFont.FreeTypeFont:
+        return ImageFont.truetype(self.font_filepath, self.fontsize)
