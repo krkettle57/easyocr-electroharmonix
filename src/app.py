fd70a6a453908bcf89504aca9c9cab6cd4cbce63
krkettle57 <krkettle57@gmail.com>
2021-10-07 11:15:42 +0900

    add main

--- src/app.py
+++ src/app.py
@@ -1,25 +1,45 @@
+from pathlib import Path
+from typing import List, Tuple
+
 import easyocr
 
 from generator import TextImageGenerator
 
-text = "HELLO, WORLD!"
-font_filepath = "fonts/electroharmonix.ttf"
-output_dir = "data"
 
-output_image_filepath = f"{output_dir}/gen_{text}.png"
+def get_ocr_detects(langs: List[List[str]], filepath: str) -> List[Tuple[str, str]]:
+    detects = []
+    for lang in langs:
+        result = easyocr.Reader(lang, gpu=False).readtext(filepath)
+        key = ",".join(lang)
+        for detect in result:
+            detects.append((key, detect[1]))
 
-generator = TextImageGenerator(font_filepath)
-generator.generate(text).save(output_image_filepath)
+    return detects
 
 
-langs = [
-    ["en"],
-    ["ja"],
-    ["en", "ja"],
-]
+def get_font_name(font_filepath: str) -> str:
+    return Path(font_filepath).stem
 
-for lang in langs:
-    result = easyocr.Reader(lang, gpu=False).readtext(output_image_filepath)
-    key = ",".join(lang)
-    for detect in result:
-        print(f"{key}: {detect[1]}")
+
+def main(text: str, font_filepath: str) -> Tuple[str, List[Tuple[str, str]]]:
+    langs = [
+        ["en"],
+        ["ja"],
+        ["en", "ja"],
+    ]
+    font_name = get_font_name(font_filepath)
+    output_dir = "data"
+
+    output_image_filepath = f"{output_dir}/gen_{font_name}_{text}.png"
+
+    generator = TextImageGenerator(font_filepath)
+    generator.generate(text).save(output_image_filepath)
+    detects = get_ocr_detects(langs, output_image_filepath)
+
+    return font_name, detects
+
+
+if __name__ == "__main__":
+    text = "HELLO, WORLD!"
+    for font_filepath in Path("fonts").glob("*.ttf"):
+        print(main(text, str(font_filepath)))
