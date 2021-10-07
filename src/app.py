0ab2931bc5fcd49075dfd2b7a921e6b0b1030d06
krkettle57 <krkettle57@gmail.com>
2021-10-07 10:45:30 +0900

    add ocr read

--- src/app.py
+++ src/app.py
@@ -1,8 +1,25 @@
+import easyocr
+
 from generator import TextImageGenerator
 
 text = "HELLO, WORLD!"
 font_filepath = "fonts/electroharmonix.ttf"
 output_dir = "data"
 
+output_image_filepath = f"{output_dir}/gen_{text}.png"
+
 generator = TextImageGenerator(font_filepath)
-generator.generate(text).save(f"{output_dir}/image.png")
+generator.generate(text).save(output_image_filepath)
+
+
+langs = [
+    ["en"],
+    ["ja"],
+    ["en", "ja"],
+]
+
+for lang in langs:
+    result = easyocr.Reader(lang, gpu=False).readtext(output_image_filepath)
+    key = ",".join(lang)
+    for detect in result:
+        print(f"{key}: {detect[1]}")
